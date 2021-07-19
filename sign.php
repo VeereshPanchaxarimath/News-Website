dcb513b99554a9702c58de06833320d5c237c544
Veeresh Panchaxarimath <61748745+VeereshPanchaxarimath@users.noreply.github.com>
2021-07-20 00:43:33 +0530

    Add files via upload
    Uploading files of news website.

---
+++ sign.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your pincode <?php echo $_GET["pin"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br/>
+<button>
+    <a href="news.html">HOME</a>
+</button>
+
+
+</body>
+</html>
